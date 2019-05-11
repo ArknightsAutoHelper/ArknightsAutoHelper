408b9cb2c4238689173510fc45683da02c151405
ninthDevilHAUNSTER <luotianhan@126.com>
2019-05-11 22:08:14 +0800

    更新分支，该分支主要用来开发ADBShell的相关功能

--- Arknights/swape_locater.py
+++ Arknights/swape_locater.py
@@ -2,13 +2,13 @@
 在这里做一些测试，包括拖动后的点击位置获取操作
 
 '''
-from Arknights import ArknightsHelper
-from Arknights.click_location import SWIPE_LOCATION, CLICK_LOCATION
+from Arknights.click_location import *
 from time import sleep
+from ADBShell import ADBShell
 
-ark = ArknightsHelper()
-ark.adb.get_mouse_swipe(SWIPE_LOCATION['BATTLE_TO_MAP_LEFT'])
-ark.adb.get_mouse_swipe(SWIPE_LOCATION['BATTLE_TO_MAP_RIGHT'])
+adb = ADBShell()
+adb.get_mouse_swipe(SWIPE_LOCATION['BATTLE_TO_MAP_LEFT'])
+adb.get_mouse_swipe(SWIPE_LOCATION['BATTLE_TO_MAP_RIGHT'])
 sleep(3)
 # ark.adb.get_mouse_click(CLICK_LOCATION["BATTLE_SELECT_MAIN_TASK_4-6"])
 sleep(200)
