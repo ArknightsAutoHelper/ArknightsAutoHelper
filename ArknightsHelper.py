e42062e7babd94a4c55a6c963002f63d51d66857
ninthDevilHAUNSTER <luotianhan@126.com>
2019-05-11 18:45:32 +0800

    把click_location 完善了一下

--- ArknightsHelper.py
+++ ArknightsHelper.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 TASK_LIST = OrderedDict()
 # TASK_LIST["S2-1"] = 2
 # TASK_LIST['2-2'] = 10
-TASK_LIST["4-4"] = 5
+# TASK_LIST["LS-5"] = 5
+TASK_LIST["CE-5"] = 5
 Ark = ArknightsHelper()
 Ark.main_handler(TASK_LIST)
