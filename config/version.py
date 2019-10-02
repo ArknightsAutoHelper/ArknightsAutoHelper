436733da9e3986baf335d679b8c506a7319c9f97
dantmnf <dantmnf@users.noreply.github.com>
2019-10-02 13:18:35 +0800

    add version config

---
+++ config/version.py
@@ -0,0 +1,13 @@
+"""
+get version from git working tree
+
+FIXME: replace with hardcoded value for release
+"""
+import subprocess
+import os
+
+_dir = os.path.dirname(__file__)
+branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir).strip()
+_description = subprocess.check_output(['git', 'describe', '--always', '--dirty'], cwd=_dir).strip()
+
+version = (b'%b+%b' % (branch, _description)).decode()
