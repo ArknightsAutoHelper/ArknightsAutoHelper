57cc31c32db16b8edba27490f9a2c997a3de2fc7
奔跑的梅洛斯 <44662261+RunningMelos@users.noreply.github.com>
2019-10-13 12:30:02 +0800

    添加收获信赖和基建产物

--- Arknights/base.py
+++ Arknights/base.py
@@ -667,3 +667,59 @@ SECRET_KEY\t{secret_key}
                     break
             screenshot = self.adb.get_screen_shoot()
         logger.info("奖励已领取完毕")
+
+    def get_credit(self):
+        logger.debug("[D] base.get_credit")
+        logger.info("[i] 领取信赖")
+        self.back_to_main()
+        screenshot = self.adb.get_screen_shoot()
+        logger.info('[i] 进入好友列表')
+        self.tap_quadrilateral(imgreco.main.get_friend_corners(screenshot))
+        self.__wait(MEDIUM_WAIT)
+        self.tap_quadrilateral(imgreco.main.get_friend_list(screenshot))
+        self.__wait(MEDIUM_WAIT)
+        logger.info('[i] 访问好友基建')
+        self.tap_quadrilateral(imgreco.main.get_friend_build(screenshot))
+        self.__wait(MEDIUM_WAIT)
+        building_count = 0
+        while building_count <= 11:
+            screenshot = self.adb.get_screen_shoot()
+            self.tap_quadrilateral(imgreco.main.get_next_friend_build(screenshot))
+            self.__wait(MEDIUM_WAIT)
+            building_count = building_count + 1
+        logger.info('[i] 信赖领取完毕')
+    
+    def get_building(self):
+        logger.debug("[D] base.get_building")
+        logger.info("[i] 清空基建")
+        self.back_to_main()
+        screenshot = self.adb.get_screen_shoot()
+        logger.info('[i] 进入我的基建')
+        self.tap_quadrilateral(imgreco.main.get_back_my_build(screenshot))
+        self.__wait(MEDIUM_WAIT + 3)
+        self.tap_quadrilateral(imgreco.main.get_my_build_task(screenshot))
+        self.__wait(SMALL_WAIT)
+        logger.info('[i] 收取制造产物')
+        self.tap_quadrilateral(imgreco.main.get_my_build_task_clear(screenshot))
+        self.__wait(SMALL_WAIT)
+        logger.info('[i] 清理贸易订单')
+        self.tap_quadrilateral(imgreco.main.get_my_sell_task_1(screenshot))
+        self.__wait(SMALL_WAIT)
+        self.tap_quadrilateral(imgreco.main.get_my_sell_tasklist(screenshot))
+        self.__wait(SMALL_WAIT)
+        sell_count = 0
+        while sell_count <= 6:
+            screenshot = self.adb.get_screen_shoot()
+            self.tap_quadrilateral(imgreco.main.get_my_sell_task_main(screenshot))
+            self.__wait(SMALL_WAIT)
+            sell_count = sell_count + 1
+        self.tap_quadrilateral(imgreco.main.get_my_sell_task_2(screenshot))
+        self.__wait(SMALL_WAIT - 1)
+        sell_count = 0
+        while sell_count <= 6:
+            screenshot = self.adb.get_screen_shoot()
+            self.tap_quadrilateral(imgreco.main.get_my_sell_task_main(screenshot))
+            self.__wait(SMALL_WAIT - 1)
+            sell_count = sell_count + 1
+        self.back_to_main()
+        logger.info("[i] 基建领取完毕")
