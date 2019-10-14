126f0e5a427fa5a17bb8a44fe8a67307577072fa
奔跑的梅洛斯 <44662261+RunningMelos@users.noreply.github.com>
2019-10-14 22:23:43 +0800

    剔除基建相关内容

--- Arknights/base.py
+++ Arknights/base.py
@@ -689,37 +689,3 @@ SECRET_KEY\t{secret_key}
             building_count = building_count + 1
         logger.info('信赖领取完毕')
     
-    def get_building(self):
-        logger.debug("base.get_building")
-        logger.info("清空基建")
-        self.back_to_main()
-        screenshot = self.adb.get_screen_shoot()
-        logger.info('进入我的基建')
-        self.tap_quadrilateral(imgreco.main.get_back_my_build(screenshot))
-        self.__wait(MEDIUM_WAIT + 3)
-        self.tap_quadrilateral(imgreco.main.get_my_build_task(screenshot))
-        self.__wait(SMALL_WAIT)
-        logger.info('收取制造产物')
-        self.tap_quadrilateral(imgreco.main.get_my_build_task_clear(screenshot))
-        self.__wait(SMALL_WAIT)
-        logger.info('清理贸易订单')
-        self.tap_quadrilateral(imgreco.main.get_my_sell_task_1(screenshot))
-        self.__wait(SMALL_WAIT + 1)
-        self.tap_quadrilateral(imgreco.main.get_my_sell_tasklist(screenshot))
-        self.__wait(SMALL_WAIT)
-        sell_count = 0
-        while sell_count <= 6:
-            screenshot = self.adb.get_screen_shoot()
-            self.tap_quadrilateral(imgreco.main.get_my_sell_task_main(screenshot))
-            self.__wait(SMALL_WAIT)
-            sell_count = sell_count + 1
-        self.tap_quadrilateral(imgreco.main.get_my_sell_task_2(screenshot))
-        self.__wait(SMALL_WAIT - 1)
-        sell_count = 0
-        while sell_count <= 6:
-            screenshot = self.adb.get_screen_shoot()
-            self.tap_quadrilateral(imgreco.main.get_my_sell_task_main(screenshot))
-            self.__wait(SMALL_WAIT - 1)
-            sell_count = sell_count + 1
-        self.back_to_main()
-        logger.info("基建领取完毕")
