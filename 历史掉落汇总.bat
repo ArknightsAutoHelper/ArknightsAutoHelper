0e6fdce51ffdd614f44e1d2a8d2d9014b8c0c6c5
奔跑的梅洛斯 <44662261+RunningMelos@users.noreply.github.com>
2019-10-14 22:30:36 +0800

    修改提取内容 并作出提示

--- 历史掉落汇总.bat
+++ 历史掉落汇总.bat
@@ -2,14 +2,18 @@
 chcp 65001>nul 2>nul
 
 echo [93m[!] 本批处理会清除历史log ！！！[1m
-set /p choice="[94m[i] 输入 'y' 确认 | 任意键退出：[0m"
+echo [93m[!] 本批处理会清除历史log ！！！[1m
+echo [93m[!] 本批处理会清除历史log ！！！[1m
+echo [93m[!] 运行过程中出现 BUG ，请先备份好对应的 log文件以便排除问题！！！[1m
+echo [93m[!] 运行过程中出现 BUG ，请先备份好对应的 log文件以便排除问题！！！[1m
+echo [93m[!] 运行过程中出现 BUG ，请先备份好对应的 log文件以便排除问题！！！[1m
+set /p choice="[94m[i] 输入 'y' 继续 | 任意键退出：[0m"
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='y' goto history
 goto end
 
 :history
-@echo --------------------------------->>物品掉落.txt
-@findstr "掉落识别结果" log\ArknightsAutoHelper.log>>物品掉落.txt
+@findstr "stars" log\ArknightsAutoHelper.log>>物品掉落.txt
 @del log\*.log*
 echo [94m[i] 已将掉落信息添加至 物品掉落.txt 文件[0m
 
