bf583b04b6176c2492aecdbfff081e9915e518e9
dantmnf <dantmnf@users.noreply.github.com>
2019-11-30 23:24:56 +0800

    hardcode version for release

--- config/version.py
+++ config/version.py
@@ -1,13 +1,1 @@
-"""
-get version from git working tree
-
-FIXME: replace with hardcoded value for release
-"""
-import os
-import subprocess
-
-_dir = os.path.dirname(__file__)
-branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir).strip()
-_description = subprocess.check_output(['git', 'describe', '--always', '--dirty'], cwd=_dir).strip()
-
-version = (b'%b+%b' % (branch, _description)).decode()
+version = 'release-20191017'
