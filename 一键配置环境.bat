976026f4d32c0699c8fbc7bde989d1abc424ac19
dantmnf <dantmnf@users.noreply.github.com>
2020-05-24 14:11:00 +0800

    use venv on batch files

--- 一键配置环境.bat
+++ 一键配置环境.bat
@@ -1,3 +1,11 @@
-@pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-
-@pip install -r requirements.txt
+@echo off
+setlocal EnableDelayedExpansion
+set PYTHON_EXECUTABLE=python3
+where /q py
+if errorlevel 0 set PYTHON_EXECUTABLE=py
+cd /d "%~dp0"
+!PYTHON_EXECUTABLE! -m venv venv
+call venv\Scripts\activate.bat
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
+endlocal
--- 整合工具箱.bat
+++ 整合工具箱.bat
@@ -3,6 +3,7 @@ chcp>nul 2>nul 65001
 rem 切换至 ArknightsAutoHelper 所在位置
 :path
 cd>nul 2>nul /D %~dp0
+call venv\Scripts\activate.bat
 
 rem 主要循环节
 :main
