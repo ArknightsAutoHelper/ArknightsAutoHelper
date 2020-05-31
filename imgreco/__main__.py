d18b7fc43e729287acc65a2095047aee663ff541
dantmnf <dantmnf@users.noreply.github.com>
2020-05-31 13:55:00 +0800

    add usage message to imgreco launcher

--- imgreco/__main__.py
+++ imgreco/__main__.py
@@ -1,11 +1,13 @@
 import sys
-from PIL import Image
-import imgreco
 
-obj = imgreco
-
-objname = '.'.join(sys.argv[1:-1])
-for k in sys.argv[1:-1]:
-    obj = getattr(obj, k)
-print('> imgreco.%s(Image.open(%s))' % (objname, repr(sys.argv[-1])))
-print(obj(Image.open(sys.argv[-1])))
+if len(sys.argv) > 1:
+    from PIL import Image
+    import imgreco
+    obj = imgreco
+    objname = '.'.join(sys.argv[1:-1])
+    for k in sys.argv[1:-1]:
+        obj = getattr(obj, k)
+    print('> imgreco.%s(Image.open(%s))' % (objname, repr(sys.argv[-1])))
+    print(obj(Image.open(sys.argv[-1])))
+else:
+    print('usage: python -m imgreco module_name function_name image_file')
