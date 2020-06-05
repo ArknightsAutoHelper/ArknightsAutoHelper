202b4c974621b1874c4c80d215d38efb5e76cf4d
dantmnf <dantmnf@users.noreply.github.com>
2020-06-05 23:48:47 +0800

    add generated penguin-stats API

---
+++ penguin_stats/gen/penguin_client/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from penguin_client.api.account_api import AccountApi
+from penguin_client.api.formula_api import FormulaApi
+from penguin_client.api.item_api import ItemApi
+from penguin_client.api.notice_api import NoticeApi
+from penguin_client.api.period_api import PeriodApi
+from penguin_client.api.report_api import ReportApi
+from penguin_client.api.result_api import ResultApi
+from penguin_client.api.stage_api import StageApi
+from penguin_client.api.website_statistics_api import WebsiteStatisticsApi
+from penguin_client.api.zone_api import ZoneApi
+from penguin_client.api._deprecated_ap_is_api import DeprecatedAPIsApi
