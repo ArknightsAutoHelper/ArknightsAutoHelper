62b76fa14512a00ba73437778aa9f3705824bd8e
dantmnf <dantmnf@users.noreply.github.com>
2020-08-02 18:21:48 +0800

    show tag in scm_version

--- config/scm_version.py
+++ config/scm_version.py
@@ -7,7 +7,7 @@ import subprocess
 try:
     _dir = os.path.dirname(__file__)
     branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir).strip()
-    _description = subprocess.check_output(['git', 'describe', '--always', '--dirty'], cwd=_dir).strip()
+    _description = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=_dir).strip()
 
     version = (b'%b+%b' % (branch, _description)).decode()
 except:
