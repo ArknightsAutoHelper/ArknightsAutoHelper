77c7c2557d8749e9da1351e424731642627e71db
dantmnf <dantmnf@users.noreply.github.com>
2020-11-24 13:23:09 +0800

    use opentuna CDN mirror

--- 一键配置环境.bat
+++ 一键配置环境.bat
@@ -6,6 +6,5 @@ if errorlevel 0 set PYTHON_EXECUTABLE=py
 cd /d "%~dp0"
 !PYTHON_EXECUTABLE! -m venv venv
 call venv\Scripts\activate.bat
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install -r requirements.txt
+pip install -r requirements.txt --index-url 
 endlocal
