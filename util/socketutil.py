9ddb5581d52d90ff504eeda2cb1d4a0ff491f44a
dantmnf <dantmnf@users.noreply.github.com>
2020-11-26 23:40:44 +0800

    use numpy uninitialized array as buffer

--- util/socketutil.py
+++ util/socketutil.py
@@ -1,35 +1,40 @@
 import socket
+import logging
+import time
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
 
 def recvexactly(sock, n):
-    buf = bytearray(n)
-    mem = memoryview(buf)
+    buf = np.empty(n, dtype=np.uint8)
     pos = 0
     while pos < n:
-        rcvlen = sock.recv_into(mem[pos:])
+        rcvlen = sock.recv_into(buf[pos:])
         pos += rcvlen
         if rcvlen == 0:
             break
     if pos != n:
         raise EOFError("recvexactly %d bytes failed" % n)
-    return bytes(buf)
+    return buf.tobytes()
 
 
-def recvall(sock, reflen=65536, return_mem=False):
-    buflen = reflen
-    buf = bytearray(buflen)
-    mem = memoryview(buf)
+def recvall(sock, chunklen=65536, return_buffer=False):
+    buffers = []
+    current_buf = np.empty(chunklen, dtype=np.uint8)
     pos = 0
     while True:
-        if pos >= buflen:
-            # do realloc
-            mem.release()
-            buf += bytearray(reflen)
-            buflen += reflen
-            mem = memoryview(buf)
-        rcvlen = sock.recv_into(mem[pos:])
+        if pos >= chunklen:
+            buffers.append(current_buf)
+            current_buf = np.empty(chunklen, dtype=np.uint8)
+            pos = 0
+        rcvlen = sock.recv_into(current_buf[pos:])
         pos += rcvlen
         if rcvlen == 0:
             break
-    if return_mem:
-        return mem[:pos]
-    return buf[:pos]
+    buffers.append(current_buf[:pos])
+    result = np.concatenate(buffers)
+    if return_buffer:
+        return result.data
+    else:
+        return result.tobytes()
