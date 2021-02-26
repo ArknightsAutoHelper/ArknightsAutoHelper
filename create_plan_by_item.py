dca553a207f9963ea5defe7a22e849f52817173e
ShadowFox <641191800@qq.com>
2021-02-26 12:20:07 +0800

    add penguin cache

--- create_plan_by_item.py
+++ create_plan_by_item.py
@@ -3,24 +3,27 @@ import json
 
 
 if __name__ == '__main__':
-    # print('正在获取材料列表...')
-    # materials = arkplanner.get_all_materials()
-    # material_index_map = {}
-    # material_id_map = {}
-    # print('材料列表：')
-    # for i in range(len(materials)):
-    #     material_index_map[i] = materials[i]
-    #     material_id_map[materials[i].item_id] = materials[i]
-    #     print('序号: %s, 材料等级: %s, 名称: %s' % (i, materials[i].rarity, materials[i].name))
-    # required = {}
-    # while True:
-    #     # s = '2/1'
-    #     s = input('材料序号/需求数量(输入 q 结束):')
-    #     if s.strip() == 'q':
-    #         break
-    #     a = s.strip().split('/')
-    #     required[a[0]] = int(a[1])
-    required = {'30125': 1}
+    cache_time = arkplanner.get_cache_time()
+    c = input('是否刷新企鹅物流缓存(缓存时间: %s)[y/N]:' % cache_time)
+    if c.lower() == 'y':
+        arkplanner.update_cache()
+    materials = arkplanner.get_all_materials()
+    material_index_map = {}
+    material_id_map = {}
+    print('材料列表：')
+    for i in range(len(materials)):
+        material_index_map[i] = materials[i]
+        material_id_map[materials[i]['itemId']] = materials[i]
+        print('序号: %s, 材料等级: %s, 名称: %s' % (i, materials[i]['rarity'], materials[i]['name']))
+    required = {}
+    while True:
+        # s = '2/1'
+        s = input('材料序号/需求数量(输入 q 结束):')
+        if s.strip() == 'q':
+            break
+        a = s.strip().split('/')
+        required[a[0]] = int(a[1])
+    # required = {'30125': 1}
     # todo 检查已有的数量
     owns = {}
     print('正在获取刷图计划...')
@@ -31,8 +34,8 @@ if __name__ == '__main__':
     print('刷图计划:')
     for stage in plan['stages']:
         stage_info = main_stage_map[stage['stage']]
-        print('关卡 [%s] 次数 %s' % (stage_info.code, stage['count']))
-        stage_task_list.append({'stage': stage_info.code, 'count': int(stage['count'])})
+        print('关卡 [%s] 次数 %s' % (stage_info['code'], stage['count']))
+        stage_task_list.append({'stage': stage_info['code'], 'count': int(stage['count'])})
     print('预计消耗理智:', plan['cost'])
     save_data = {
         'required': required,
--- penguin_stats/arkplanner.py
+++ penguin_stats/arkplanner.py
@@ -1,25 +1,68 @@
-import penguin_client
 from functools import lru_cache
 import requests
+import os
+import json
+from datetime import datetime
 
-item_api = penguin_client.ItemApi()
-stage_api = penguin_client.StageApi()
+penguin_base_url = 'https://penguin-stats.io/PenguinStats'
+cache_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'penguin_cache.json')
+
+
+def update_cache():
+    cache = {
+        'cache_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'items': get_all_items_from_api(),
+        'stages': get_all_stages_from_api()
+    }
+    with open(cache_path, 'w') as f:
+        json.dump(cache, f)
+    load_cache.cache_clear()
+    return load_cache()
+
+
+@lru_cache(1)
+def load_cache():
+    if os.path.exists(cache_path):
+        with open(cache_path, 'r') as f:
+            return json.load(f)
+    else:
+        return update_cache()
+
+
+def get_all_items_from_api():
+    return requests.get(penguin_base_url + '/api/v2/items').json()
+
+
+def get_all_stages_from_api():
+    return requests.get(penguin_base_url + '/api/v2/stages').json()
+
+
+def get_all_items():
+    return load_cache()['items']
+
+
+def get_all_stages():
+    return load_cache()['stages']
+
+
+def get_cache_time():
+    return load_cache()['cache_time']
 
 
 @lru_cache(1)
 def get_all_materials():
-    items = item_api.get_all_items_using_get1()
-    materials = list(filter(lambda x: x.item_type == 'MATERIAL', items))
-    return sorted(materials, key=lambda k: k.rarity, reverse=True)
+    items = get_all_items()
+    materials = list(filter(lambda x: x['itemType'] == 'MATERIAL', items))
+    return sorted(materials, key=lambda k: k['rarity'], reverse=True)
 
 
 @lru_cache(1)
 def get_main_stage_map():
-    stages = stage_api.get_all_stages_using_get1()
-    main_stages = list(filter(lambda x: x.stage_type == 'MAIN', stages))
+    stages = get_all_stages()
+    main_stages = list(filter(lambda x: x['stageType'] == 'MAIN', stages))
     stage_map = {}
     for stage in main_stages:
-        stage_map[stage.stage_id] = stage
+        stage_map[stage['stageId']] = stage
     return stage_map
 
 
