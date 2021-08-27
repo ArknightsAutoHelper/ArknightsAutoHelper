42a3f63e0a952860598cb3c8d08b058048f778d6
triwinds <7413161+triwinds@users.noreply.github.com>
2021-08-27 20:08:48 +0800

    add cnocr

--- 一键配置环境.bat
+++ 一键配置环境.bat
@@ -7,5 +7,5 @@ cd /d "%~dp0"
 !PYTHON_EXECUTABLE! -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-pip install -r requirements.txt --index-url https://mirrors.aliyun.com/pypi/simple
+pip install -r requirements-ex.txt --index-url https://mirrors.aliyun.com/pypi/simple
 endlocal
