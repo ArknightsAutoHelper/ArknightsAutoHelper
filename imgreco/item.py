3d18bf5fe5cc3cd438f4dfc7c5894b82a3a8275d
dantmnf <2252500+dantmnf@users.noreply.github.com>
2021-09-07 15:42:29 +0800

    use exist_ok instead of checking existence

--- imgreco/item.py
+++ imgreco/item.py
@@ -51,8 +51,7 @@ def retry_get(url, max_retry=5, timeout=3):
 
 def update_net():
     local_cache_time = 0
-    if os.path.exists(os.path.dirname(index_file)) == False:
-        os.makedirs(os.path.dirname(index_file))
+    os.makedirs(os.path.dirname(index_file), exist_ok=True)
     if os.path.exists(index_file):
         with open(index_file, 'r', encoding='utf-8') as f:
             local_rel = json.load(f)
