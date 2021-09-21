46dd2d24e4677024677d557abf46acff0966fe1a
dantmnf <dantmnf@users.noreply.github.com>
2021-09-21 16:29:40 +0800

    make git happy

--- 一键配置环境.bat
+++ 一键配置环境.bat
@@ -6,7 +6,7 @@ if errorlevel 0 set PYTHON_EXECUTABLE=py
 cd /d "%~dp0"
 !PYTHON_EXECUTABLE! -m venv venv
 call venv\Scripts\activate.bat
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip wheel setuptools
 pip install cnocr==1.2.3.1 --index-url https://mirrors.aliyun.com/pypi/simple
 pip install -r requirements-ex.txt --index-url https://mirrors.aliyun.com/pypi/simple
 endlocal
