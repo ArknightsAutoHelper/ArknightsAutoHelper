120f828c5b19ffe30487ed3409a66e2afa9240ab
dantmnf <dantmnf@users.noreply.github.com>
2021-10-02 23:26:17 +0800

    make scm_version less annoying

--- config/scm_version.py
+++ config/scm_version.py
@@ -4,11 +4,15 @@ get version from git working tree
 import os
 import subprocess
 
-try:
-    _dir = os.path.dirname(__file__)
-    branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir).strip()
-    _description = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=_dir).strip()
+version = os.environ.get('_AAH_SCM_VERSION_CACHE', None)
+if version is None:
+    try:
+        _dir = os.path.dirname(__file__)
+        _CREATE_NO_WINDOW = getattr(subprocess, 'CREATE_NO_WINDOW', 0)
+        branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
+        _description = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
 
-    version = (b'%b+%b' % (branch, _description)).decode()
-except:
-    version = 'UNKNOWN'
+        version = (b'%b+%b' % (branch, _description)).decode()
+    except:
+        version = 'UNKNOWN'
+    os.environ['_AAH_SCM_VERSION_CACHE'] = version
