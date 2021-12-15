7445401a449e384aaa628a3aecc704a86f15a6cd
dantmnf <dantmnf@users.noreply.github.com>
2021-12-15 13:18:18 +0800

    update scm_version scheme

--- config/scm_version.py
+++ config/scm_version.py
@@ -10,9 +10,10 @@ if version is None:
         _dir = os.path.dirname(__file__)
         _CREATE_NO_WINDOW = getattr(subprocess, 'CREATE_NO_WINDOW', 0)
         branch = subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD'), cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
-        _description = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
+        _count = subprocess.check_output(['git', 'rev-list', '--count', 'HEAD'], cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
+        _description = subprocess.check_output(['git', 'describe', '--always', '--dirty', '--exclude=*'], cwd=_dir, creationflags=_CREATE_NO_WINDOW).strip()
 
-        version = (b'%b+%b' % (branch, _description)).decode()
+        version = (b'%b-%b+%b' % (branch, _count, _description)).decode()
     except:
         version = 'UNKNOWN'
     os.environ['_AAH_SCM_VERSION_CACHE'] = version
