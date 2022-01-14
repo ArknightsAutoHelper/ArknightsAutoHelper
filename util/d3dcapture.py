27cc023e3495cfa5ef1594dbf53356bd58d1d231
dantmnf <dantmnf@users.noreply.github.com>
2022-01-14 21:40:20 +0800

    reduce redundant texture creation

--- util/d3dcapture.py
+++ util/d3dcapture.py
@@ -25,6 +25,7 @@ class CaptureSession:
         self._last_size = None
         self.frame_callback = None
         self.close_callback = None
+        self.cputex = None
 
     def _create_device(self):
         d3d11.D3D11CreateDevice(
@@ -41,6 +42,7 @@ class CaptureSession:
         )
         self._rtdevice = CreateDirect3D11DeviceFromDXGIDevice(self._dxdevice)
         self._evtoken = None
+
     def start(self, hwnd, capture_cursor=False):
         self.stop()
         self._create_device()
@@ -48,6 +50,7 @@ class CaptureSession:
         item = interop.CreateForWindow(hwnd, rotypes.Windows.Graphics.Capture.IGraphicsCaptureItem.GUID)
         self._item = item
         self._last_size = item.Size
+        self._reset_cputex(item.Size)
         delegate = TypedEventHandler(rotypes.Windows.Graphics.Capture.GraphicsCaptureItem, IInspectable).delegate(self._closed_callback)
         self._evtoken = item.add_Closed(delegate)
         self._framepool = rotypes.Windows.Graphics.Capture.Direct3D11CaptureFramePool.CreateFreeThreaded(self._rtdevice, rotypes.Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, item.Size)
@@ -62,9 +65,9 @@ class CaptureSession:
             self.frame_callback(self)
 
     def _closed_callback(self, x, y):
+        self.stop()
         if self.close_callback is not None:
             self.close_callback(self)
-        self.stop()
 
     def stop(self):
         if self._framepool is not None:
@@ -76,12 +79,31 @@ class CaptureSession:
         self._item = None
         self._rtdevice.Release()
         self._dxdevice.Release()
+        if self.cputex:
+            self.cputex.Release()
+
+    def _reset_cputex(self, size):
+        if self.cputex is not None:
+            self.cputex.Release()
+        desc2 = d3d11.D3D11_TEXTURE2D_DESC()
+        desc2.Width = size.Width
+        desc2.Height = size.Height
+        desc2.MipLevels = 1
+        desc2.ArraySize = 1
+        desc2.Format = d3d11.DXGI_FORMAT_B8G8R8A8_UNORM
+        desc2.SampleDesc = d3d11.DXGI_SAMPLE_DESC(Count=1, Quality=0)
+        desc2.Usage = d3d11.D3D11_USAGE_STAGING
+        desc2.CPUAccessFlags = d3d11.D3D11_CPU_ACCESS_READ
+        desc2.BindFlags = 0
+        desc2.MiscFlags = 0
+        self.cputex_desc = desc2
+        self.cputex = self._dxdevice.CreateTexture2D(ctypes.byref(desc2), None)
 
     def _reset_framepool(self, size, reset_device=False):
         if reset_device:
             self._create_device()
         self._framepool.Recreate(self._rtdevice, rotypes.Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, size)
-
+        self._reset_cputex(size)
 
     def get_frame(self):
         frame = self._framepool.TryGetNextFrame()
@@ -100,27 +122,15 @@ class CaptureSession:
                 self._reset_framepool(frame.ContentSize)
                 return self.get_frame()
             tex = None
-            cputex = None
             try:
                 tex = frame.Surface.astype(IDirect3DDxgiInterfaceAccess).GetInterface(d3d11.ID3D11Texture2D.GUID).astype(d3d11.ID3D11Texture2D)
-                desc = tex.GetDesc()
-                desc2 = d3d11.D3D11_TEXTURE2D_DESC()
-                desc2.Width = desc.Width
-                desc2.Height = desc.Height
-                desc2.MipLevels = desc.MipLevels
-                desc2.ArraySize = desc.ArraySize
-                desc2.Format = desc.Format
-                desc2.SampleDesc = desc.SampleDesc
-                desc2.Usage = d3d11.D3D11_USAGE_STAGING
-                desc2.CPUAccessFlags = d3d11.D3D11_CPU_ACCESS_READ
-                desc2.BindFlags = 0
-                desc2.MiscFlags = 0
-                cputex = self._dxdevice.CreateTexture2D(ctypes.byref(desc2), None)
-                self._immediatedc.CopyResource(cputex, tex)
-                mapinfo = self._immediatedc.Map(cputex, 0, d3d11.D3D11_MAP_READ, 0)
+                # desc = tex.GetDesc()
+                desc = self.cputex_desc
+                self._immediatedc.CopyResource(self.cputex, tex)
+                mapinfo = self._immediatedc.Map(self.cputex, 0, d3d11.D3D11_MAP_READ, 0)
                 mat = np.ctypeslib.as_array(ctypes.cast(mapinfo.pData, PBYTE), (desc.Height, mapinfo.RowPitch // 4, 4))[:, :desc.Width].copy()
                 img = Image.fromarray(mat, 'BGRA')
-                self._immediatedc.Unmap(cputex, 0)
+                self._immediatedc.Unmap(self.cputex, 0)
             except OSError as e:
                 if e.winerror == d3d11.DXGI_ERROR_DEVICE_REMOVED or e.winerror == d3d11.DXGI_ERROR_DEVICE_RESET:
                     need_reset_framepool = True
@@ -130,8 +140,6 @@ class CaptureSession:
             finally:
                 if tex is not None:
                     tex.Release()
-                if cputex is not None:
-                    cputex.Release()
             if need_reset_framepool:
                     self._reset_framepool(frame.ContentSize, need_reset_device)
                     return self.get_frame()
@@ -140,8 +148,9 @@ class CaptureSession:
 def test():
     import time
     import cv2
-    # hwnd = ctypes.windll.user32.FindWindowW('com.android.documentsui', None)  # Documents UI window of Windows Subsystem for Android
-    hwnd = ctypes.windll.user32.FindWindowW('CabinetWClass', None)  # random explorer window
+    print(ctypes.windll.user32.SetThreadDpiAwarenessContext(ctypes.c_ssize_t(-4)))
+    hwnd = ctypes.windll.user32.FindWindowW('com.android.settings', None)  # Documents UI window of Windows Subsystem for Android
+    # hwnd = ctypes.windll.user32.FindWindowW('CabinetWClass', None)  # random explorer window
     title = f"screenshot for window {hwnd}"
     cv2.namedWindow(title)
     print(title)
@@ -157,7 +166,7 @@ def test():
         state_box[2] = True
     session.frame_callback = frame_callback
     session.close_callback = close_callback
-    session.start(hwnd, True)
+    session.start(hwnd, False)
     # time.sleep(2)
     # t0 = time.perf_counter()
     # frame = session.get_frame()
