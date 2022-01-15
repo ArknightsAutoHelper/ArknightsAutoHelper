dd60844ffce83cf3b76128ae650b5251078683d0
chunibyo-wly <chunibyo.wly@gmail.com>
2022-01-15 23:08:36 +0800

    add: 招募完成

--- Arknights/addons/roguelike.py
+++ Arknights/addons/roguelike.py
@@ -2,7 +2,7 @@ from random import randint
 
 import imgreco.roguelike
 from Arknights.addons.common import CommonAddon
-from Arknights.flags import SMALL_WAIT, TINY_WAIT
+from Arknights.flags import SMALL_WAIT, TINY_WAIT, MEDIUM_WAIT
 from automator import AddonBase
 
 
@@ -44,8 +44,64 @@ class RoguelikeAddon(AddonBase):
 
         self.logger.info("近卫招募")
         self.tap_rect(self.ocr.INITIAL_RECRUIT_BUTTON[0])
+        self.delay(TINY_WAIT)
         self.logger.info("好友助战")
         self.tap_rect(self.ocr.SUPPORT_UNITS_BUTTON)
+        self.delay(TINY_WAIT)
+
+        # TODO: refresh
+        # 左半屏幕
+        screenshot = self.device.screenshot().convert('RGB')
+        subarea1 = (0, 0, screenshot.width * 0.8, screenshot.height)
+        cropped_img = screenshot.crop(subarea1)
+        if not self.ocr.check_mountain_exist(cropped_img):
+            move = -randint(self.viewport[0] // 4, self.viewport[0] // 3)
+            self.swipe_screen(move)
+            self.delay(TINY_WAIT)
+            # 右半屏幕
+            screenshot = self.device.screenshot().convert('RGB')
+            if not self.ocr.check_mountain_exist(screenshot):
+                self.logger.error("山未找到")
+                return
+
+        self.logger.info("山")
+        mountain_position = self.ocr.MOUNTAIN
+        w, h = mountain_position[2] - mountain_position[0], mountain_position[3] - mountain_position[1]
+        click_area = (mountain_position[0] + w * 2.0, mountain_position[1],
+                      mountain_position[0] + w * 2.5, mountain_position[3])
+        cropped_img = screenshot.crop(click_area)
+        if not self.ocr.check_mountain_ok(cropped_img):
+            return
+        self.ocr.MOUNTAIN_OK = (self.ocr.MOUNTAIN_OK[0] + click_area[0],
+                                self.ocr.MOUNTAIN_OK[1] + click_area[1],
+                                self.ocr.MOUNTAIN_OK[2] + click_area[0],
+                                self.ocr.MOUNTAIN_OK[3] + click_area[1])
+        # 招募
+        self.tap_rect(self.ocr.MOUNTAIN_OK)
+        self.delay(TINY_WAIT)
+        self.tap_rect((772, 499, 933, 544))
+        self.delay(MEDIUM_WAIT)
+        self.tap_rect((772, 499, 933, 544))
+
+        self.logger.info("辅助招募")
+        self.tap_rect(self.ocr.INITIAL_RECRUIT_BUTTON[1])
+        self.delay(TINY_WAIT)
+        self.tap_rect(self.ocr.RECRUIT_CONFIRM)
+        self.tap_rect(self.ocr.RECRUIT_CONFIRM2)
+        self.delay(TINY_WAIT)
+
+        self.logger.info("医疗招募")
+        self.tap_rect(self.ocr.INITIAL_RECRUIT_BUTTON[2])
+        self.delay(TINY_WAIT)
+        self.tap_rect(self.ocr.RECRUIT_CONFIRM)
+        self.tap_rect(self.ocr.RECRUIT_CONFIRM2)
+        self.delay(TINY_WAIT)
+
+        screenshot = self.device.screenshot().convert('RGB')
+        if not self.ocr.check_enter_castle_exist(screenshot):
+            return
+        self.tap_rect(self.ocr.ENTER_CASTLE)
+        self.delay(MEDIUM_WAIT)
 
     def cli_roguelike(self, argv) -> int:
         """
--- imgreco/roguelike.py
+++ imgreco/roguelike.py
@@ -12,6 +12,7 @@ from util.richlog import get_logger
 logger = get_logger(__name__)
 
 
+# TODO: add unit tests
 class RoguelikeOCR:
     def __init__(self):
         self.EXPLORE_BUTTON = (1065, 515, 1263, 704)
@@ -20,6 +21,11 @@ class RoguelikeOCR:
         self.STRENGTHS_MAKE_UP_WEAKNESS_BUTTON = (655, 566, 912, 632)
         self.INITIAL_RECRUIT_BUTTON = [(243, 522, 460, 574), (534, 521, 751, 573), (821, 521, 1038, 573)]
         self.SUPPORT_UNITS_BUTTON = (1012, 10, 1187, 57)
+        self.MOUNTAIN = (0, 0, 0, 0)
+        self.MOUNTAIN_OK = (0, 0, 0, 0)
+        self.RECRUIT_CONFIRM = (1069, 654, 1251, 699)
+        self.RECRUIT_CONFIRM2 = (732, 457, 936, 529)
+        self.ENTER_CASTLE = (0, 0, 0, 0)
 
     def check_explore_button_exist(self, img):
         """
@@ -34,10 +40,13 @@ class RoguelikeOCR:
         return mse < 500
 
     def check_assault_detachment(self, img):
+        """
+        选择突击战术分队
+        """
         img = img.convert('RGB')
         tmp, score = self._get_rect_by_template(img, "assault")
         logger.logimage(img.crop(tmp))
-        logger.logtext('score=%f' % score)
+        logger.logtext('assault score=%f' % score)
         if score > 0.9:
             self.ASSAULT_BUTTON = tmp
             return True
@@ -45,14 +54,60 @@ class RoguelikeOCR:
             return False
 
     def check_assault_ok(self, img):
+        # 突击战术分队确认按钮
         subarea = (self.ASSAULT_BUTTON[0], 0, self.ASSAULT_BUTTON[2], img.height)
         cropped_img = img.convert('RGB').crop(subarea)
         tmp, score = self._get_rect_by_template(cropped_img, "assault-ok")
         tmp = (tmp[0] + self.ASSAULT_BUTTON[0], tmp[1], tmp[2] + self.ASSAULT_BUTTON[0], tmp[3])
         logger.logimage(img.crop(tmp))
-        logger.logtext('score=%f' % score)
+        logger.logtext('assault ok score=%f' % score)
         if score > 0.9:
             self.ASSAULT_OK_BUTTON = tmp
+            return True
+        else:
+            return False
+
+    def check_mountain_exist(self, img):
+        tmp, score = self._get_rect_by_template(img, "mountain")
+        logger.logimage(img.crop(tmp))
+        logger.logtext('mountain score=%f' % score)
+        if score > 0.9:
+            self.MOUNTAIN = tmp
+            return True
+        else:
+            tmp, score = self._get_rect_by_template(img, "mountain2")
+            logger.logimage(img.crop(tmp))
+            logger.logtext('mountain2 score=%f' % score)
+            if score > 0.9:
+                self.MOUNTAIN = tmp
+                return True
+        return False
+
+    def check_mountain_ok(self, img):
+        tmp, score = self._get_rect_by_template(img, "mountain-ok")
+        logger.logimage(img.crop(tmp))
+        logger.logtext('mountain-ok score=%f' % score)
+        if score > 0.9:
+            self.MOUNTAIN_OK = tmp
+            return True
+        else:
+            tmp, score = self._get_rect_by_template(img, "mountain-ok2")
+            logger.logimage(img.crop(tmp))
+            logger.logtext('mountain-ok2 score=%f' % score)
+            if score > 0.9:
+                self.MOUNTAIN_OK = tmp
+                return True
+        return False
+
+    def check_enter_castle_exist(self, img):
+        """
+        进入古堡按钮
+        """
+        tmp, score = self._get_rect_by_template(img, "enter-castle")
+        logger.logimage(img.crop(tmp))
+        logger.logtext('enter-castle score=%f' % score)
+        if score > 0.9:
+            self.ENTER_CASTLE = tmp
             return True
         else:
             return False
@@ -68,9 +123,15 @@ class RoguelikeOCR:
 def main():
     ocr = RoguelikeOCR()
     from PIL import Image
-    ocr.ASSAULT_BUTTON = (593, 169, 732, 347)
-    img = Image.open(r"C:\Users\chunibyo\Documents\MuMu共享文件夹\MuMu20220115190005.png")
-    ocr.check_assault_ok(img)
+    screenshot = Image.open(r"C:\Users\chunibyo\Documents\MuMu共享文件夹\MuMu20220115215238.png").convert('RGB')
+    subarea1 = (928.0, 506.0, 1018.0, 686.0)
+    screenshot = screenshot.convert('RGB').crop(subarea1)
+    ocr.check_mountain_ok(screenshot)
+    # mountain_position = ocr.MOUNTAIN
+    # w, h = mountain_position[2] - mountain_position[0], mountain_position[3] - mountain_position[1]
+    # click_area = (mountain_position[0] + w * 2.0, mountain_position[1],
+    #               mountain_position[0] + w * 2.5, mountain_position[3])
+    # print(click_area)
 
 
 if __name__ == '__main__':
