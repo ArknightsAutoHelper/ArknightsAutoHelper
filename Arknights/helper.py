be202e1a7dad75c8e5e376e5c4a74cc6f00ef310
dantmnf <dantmnf@users.noreply.github.com>
2022-08-13 22:01:37 +0800

    cleanup legacy device config

--- Arknights/helper.py
+++ Arknights/helper.py
@@ -18,12 +18,12 @@ class ArknightsHelper(BaseAutomator):
             body = '需要宽高比等于或大于 16∶9，且渲染高度不小于 720。'
             details = None
             if Fraction(self.viewport[1], self.viewport[0]) >= Fraction(16, 9):
-                body = '屏幕截图可能需要旋转，请尝试在 device-config 中指定旋转角度。'
-                img = self._controller.screenshot()
-                imgfile = app.screenshot_path.joinpath('orientation-diagnose-%s.png' % time.strftime("%Y%m%d-%H%M%S"))
-                img.save(imgfile)
-                import json
-                details = '参考 %s 以更正 device-config.json[%s]["screenshot_rotate"]' % (imgfile, json.dumps(self._controller.config_key))
+                body = '屏幕可能需要旋转，请尝试调整设置。'
+                # img = self._controller.screenshot()
+                # imgfile = app.screenshot_path.joinpath('orientation-diagnose-%s.png' % time.strftime("%Y%m%d-%H%M%S"))
+                # img.save(imgfile)
+                # import json
+                # details = '参考 %s 以更正 device-config.json[%s]["screenshot_rotate"]' % (imgfile, json.dumps(self._controller.config_key))
             for msg in [title, body, details]:
                 if msg is not None:
                     logger.warn(msg)
