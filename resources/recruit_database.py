32cfe0378bf1f233a9e3b7e0cd229e04025456b4
triwinds <triwinds@users.noreply.github.com>
2022-08-14 23:50:31 +0800

    get recruit database by game data

--- resources/recruit_database.py
+++ resources/recruit_database.py
@@ -1,4 +1,12 @@
-recruit_database = [
+import re
+from Arknights.addons.contrib.common_cache import load_game_data
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+recruit_database_bak = [
     ('Lancet-2', 0, ['医疗干员', '远程位', '治疗', '支援机械']),
     ('Castle-3', 0, ['近卫干员', '近战位', '支援', '支援机械']),
     ('夜刀', 1, ['先锋干员', '近战位', '新手']),
@@ -33,7 +41,6 @@ recruit_database = [
     ('砾', 3, ['特种干员', '近战位', '快速复活', '防护']),
     ('暗索', 3, ['特种干员', '近战位', '位移']),
     ('末药', 3, ['医疗干员', '远程位', '治疗']),
-    ('嘉维尔', 3, ['医疗干员', '远程位', '治疗']),
     ('调香师', 3, ['医疗干员', '远程位', '治疗']),
     ('角峰', 3, ['重装干员', '近战位', '防护']),
     ('蛇屠箱', 3, ['重装干员', '近战位', '防护']),
@@ -105,10 +112,123 @@ recruit_database = [
     ('正义骑士号', 0, ['狙击干员', '远程位', '支援', '支援机械']),
     ('刻俄柏', 5, ['术师干员', '远程位', '输出', '控场']),
     ('惊蛰', 4, ['术师干员', '远程位', '输出']),
-    ('风笛', 5, ['先锋干员', '近战位', '输出']),
+    ('风笛', 5, ['先锋干员', '近战位', '费用回复', '输出']),
     ('慑砂', 4, ['狙击干员', '远程位', '群攻', '削弱']),
     ('宴', 3, ['近卫干员', '近战位', '输出', '生存']),
     ('刻刀', 3, ['近卫干员', '近战位', '爆发', '输出']),
     ('巫恋', 4, ['辅助干员', '远程位', '削弱']),
     ('傀影', 5, ['特种干员', '近战位', '快速复活', '控场', '输出']),
 ]
+
+
+def get_all_recruit_characters():
+    recruit_re = re.compile(r'(★+)\\n(.*?)(\n|$)')
+    recruit_detail = load_game_data('gacha_table')['recruitDetail']
+    # print(recruit_detail)
+    result = recruit_re.findall(recruit_detail, re.MULTILINE)
+    return parse_match_groups(result)
+
+
+def parse_match_groups(groups):
+    result = []
+    for group in groups:
+        # star_count = len(group[0])
+        characters = []
+        for character in group[1].split(' / '):
+            characters.append(character.replace('<@rc.eml>', '').replace('</>', ''))
+        result += characters
+        # print(star_count, characters)
+    return result
+
+
+def get_character_name_map():
+    characters = load_game_data('character_table').values()
+    result = {}
+    # position = set()
+    # profession = set()
+    for character in characters:
+        result[character['name']] = character
+    #     position.add(character['position'])
+    #     profession.add(character['profession'])
+    # print(position)
+    # print(profession)
+    return result
+
+
+profession_map = {
+    'MEDIC': '医疗干员',
+    'WARRIOR': '近卫干员',
+    'PIONEER': '先锋干员',
+    'CASTER': '术师干员',
+    'SPECIAL': '特种干员',
+    'SUPPORT': '辅助干员',
+    'TANK': '重装干员',
+    'SNIPER': '狙击干员',
+}
+position_map = {
+    'MELEE': ['近战位'],
+    'RANGED': ['远程位'],
+    'ALL': ['近战位', '远程位'],
+}
+
+
+def get_recruit_database_by_game_data():
+    all_recruit_characters = get_all_recruit_characters()
+    character_name_map = get_character_name_map()
+    result = []
+    for character_name in all_recruit_characters:
+        character = character_name_map.get(character_name)
+        if not character:
+            logger.warning('character not found: %s', character_name)
+            continue
+        tag_list = [profession_map.get(character['profession'])]
+        tag_list += position_map.get(character['position'], [])
+        tag_list += character['tagList']
+        # print((character_name, character['rarity'], tag_list))
+        if character['rarity'] == 0:
+            tag_list.append('支援机械')
+        result.append((character_name, character['rarity'], tag_list))
+    return result
+
+
+def check_data():
+    recruit_database = get_recruit_database_by_game_data()
+    recruit_database_map = {}
+    for character_name, rarity, tag_list in recruit_database:
+        recruit_database_map[character_name] = (rarity, tag_list)
+    old_recruit_database_map = {}
+    for character_name, rarity, tag_list in recruit_database_bak:
+        old_recruit_database_map[character_name] = (rarity, tag_list)
+    print('=' * 20, 1)
+    check_data_map(recruit_database_map, old_recruit_database_map)
+    print('=' * 20, 2)
+    check_data_map(old_recruit_database_map, recruit_database_map)
+
+
+def check_data_map(data_map1, data_map2):
+    for character_name in data_map1:
+        data1 = data_map1[character_name]
+        data2 = data_map2.get(character_name)
+        if not data2:
+            print('no backup data:', character_name, data1)
+            continue
+        if data1[0] != data2[0]:
+            print('rarity not match:', character_name, data1, data2)
+        if data1[1][:2] != data2[1][:2]:
+            print('position/profession not match:', character_name, data1, data2)
+        if sorted(data1[1][2:]) != sorted(data2[1][2:]):
+            print('tag_list not match:', character_name, data1, data2)
+
+
+try:
+    recruit_database = get_recruit_database_by_game_data()
+except Exception as e:
+    logger.exception(e)
+    recruit_database = recruit_database_bak
+
+
+__all__ = ['recruit_database']
+
+
+if __name__ == '__main__':
+    check_data()
